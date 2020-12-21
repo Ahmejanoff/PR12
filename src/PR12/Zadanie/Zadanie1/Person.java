485702a8827d311da3bca7de142d718919eab66b
Ahmejanoff <e.ahmejanoff@gmail.com>
2020-12-21 15:27:47 +0300

    Initial commit

---
+++ src/PR12/Zadanie/Zadanie1/Person.java
@@ -0,0 +1,42 @@
+package PR12.Zadanie.Zadanie1;
+
+//Разработать класс Person, в котором имеется функция,
+// возвращающая Фамилию И.О. Функция должна учитывать возможность отсутствия
+// значений в полях Имя и Отчество.
+public class Person
+{
+    private String name,surname,patronymic;
+
+    Person(String surname)
+    {
+        this.surname=surname;
+    }
+
+    Person(String surname,String name,String patronymic)
+    {
+        this.surname=surname;
+        this.name=name;
+        this.patronymic=patronymic;
+    }
+    public String toString()
+    {
+        if(name==null || patronymic==null)
+        {
+            return(surname);
+        }
+        else
+        {
+            return(surname+" "+name+" "+patronymic);
+
+        }
+    }
+
+    public static void main(String []args)
+    {
+        Person people=new Person("Ахмеджанов","Эрик","Тимурович");
+        System.out.println(people.toString());
+        Person people1=new Person("Волков");
+        System.out.println(people1.toString());
+    }
+
+}
